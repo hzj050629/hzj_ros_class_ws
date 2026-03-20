d59a09f844ac19bb9b0917d70f2cb122a1691257
hzj050629 <3121946077@qq.com>
2026-03-21 01:11:01 +0800

    初始化ROS工作空间，包含基础功能包结构

---
+++ src/my_class_pkg/scripts/ros_publisher_node.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import String
+rospy.init_node('my_publisher')
+# 定义一个发布者对象
+pub = rospy.Publisher('my_topic', String, queue_size=10)
+# 定义一个发布频率，下面的1代表间隔1秒
+rate = rospy.Rate(1)
+while not rospy.is_shutdown():
+# 创建一个消息对象并填充数据
+	msg = String()
+	msg.data = 'Hello, world!'
+# 发布消息
+	pub.publish(msg)
+#沉睡1秒
+	rate.sleep()
